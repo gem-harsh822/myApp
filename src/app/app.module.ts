da01f1f222a95028ecd4a92aa5c2a01d9c6e3784
gem-harsh822 <Harsh.Gautam@Geminisolutions.com>
2022-07-22 17:29:42 +0530

    Added camera functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,14 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SQLite,SQLiteObject} from '@awesome-cordova-plugins/sqlite/ngx';
-import { ImagePicker } from '@awesome-cordova-plugins/image-picker/ngx';
 import { Crop } from '@ionic-native/crop/ngx';
-import { File } from '@ionic-native/file';
+import {Camera} from '@awesome-cordova-plugins/camera/ngx'
+import { ImagePicker } from '@awesome-cordova-plugins/image-picker/ngx';
+import { File } from '@awesome-cordova-plugins/file/ngx';
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [SQLite,ImagePicker,Crop,{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [SQLite,ImagePicker,Crop,Camera,File,{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,6 +2,12 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { SQLite, SQLiteObject } from '@awesome-cordova-plugins/sqlite/ngx';
 import { CrudService } from '../crud.service';
+import { ActionSheetController } from '@ionic/angular';
+import { ImagePicker ,ImagePickerOptions} from '@awesome-cordova-plugins/image-picker/ngx';
+import { Crop } from '@ionic-native/crop/ngx';
+import { Camera , CameraOptions } from '@awesome-cordova-plugins/camera/ngx';
+import { File } from '@awesome-cordova-plugins/file/ngx';
+import { DomSanitizer } from '@angular/platform-browser';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -13,11 +19,20 @@ export class HomePage {
   name: string = '';
   email: string = '';
   dob: Date;
+  imageurl: any;
+  securepath: any = window;
+  url: any;
 
   constructor(
     private sqlite: SQLite,
     private fb: FormBuilder,
-    private crud: CrudService
+    private crud: CrudService,
+    private actionsheet: ActionSheetController,
+    private camera: Camera,
+    private file:File,
+    private imagepicker:ImagePicker,
+    private crop:Crop,
+    private domsanitize:DomSanitizer
   ) {
     this.form = this.fb.group({
       name: [null, [Validators.required, Validators.minLength(5)]],
@@ -43,19 +58,83 @@ export class HomePage {
     this.email = this.form.value.email;
     this.dob = this.form.value.dob;
     console.log(this.form.value);
-    
+
     this.createUser();
   }
   showDetails() {
     this.crud.getAllUsers();
     // console.log(this.crud.getAllUsers());
-    
   }
 
   createUser() {
     // console.log("fdsfdf"+this.name, this.email, this.dob);
-    
+
     this.crud.addItem(this.name, this.email, this.dob);
+  }
+  async selectimageOptions() {
+    const actionsheet = await this.actionsheet.create({
+      header: 'Select image Source',
+      buttons: [
+        {
+          text: 'Load from Gallery',
+          handler: () => {
+            this.pickImagesFromLibrary();
+            console.log('Image Selected from Gallery');
+          },
+        },
+        {
+          text: 'Select Camera',
+          handler: () => {
+            this.chooseFromCamera(this.camera.PictureSourceType.CAMERA);
+            console.log('Camera Selected');
+          },
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel',
+        },
+      ],
+    });
+    await actionsheet.present();
+  }
+
+  chooseFromCamera(sourceType){
+    const options: CameraOptions = {
+       quality: 100,
+       mediaType: this.camera.MediaType.PICTURE,
+       destinationType: this.camera.DestinationType.FILE_URI,
+       encodingType: this.camera.EncodingType.JPEG,
+       sourceType: sourceType,
+    };
+
+    this.camera.getPicture(options).then((result) => {
+      console.log('Camera URL',result);
+      // this.imageurl = result;
+      this.imageurl = this.securepath.Ionic.WebView.convertFileSrc(result);
+      // this.imageurl = 'data:image/jpeg;base64,'+result;
+    }, error=>{
+      console.log('Error CAMERA', error);
+    });
+  }
+
+  santizeUrl(imageUrl){
+    return this.domsanitize.bypassSecurityTrustUrl(imageUrl);
+  }
+
+  pickImagesFromLibrary(){
+    const options: ImagePickerOptions = {
+      quality: 100,
+      maximumImagesCount: 1,
+    };
+    this.imagepicker.getPictures(options).then((imageresult)=> {
+    console.log('image Picker Results', imageresult);
+
+     for(let i=0; i<imageresult.length; i++){
+      this.url = this.securepath.Ionic.WebView.convertFileSrc(imageresult[i]);
+     }
+    }, rror=>{
+      console.log('Image Picker Error:', rror);
+    });
   }
 
   // remove(user) {
